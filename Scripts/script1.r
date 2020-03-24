b42a51ef88c3e35777e10ff528858d259e6a819f
karolskoczyk <32647007+karolskoczyk@users.noreply.github.com>
2020-03-24 12:54:55 +0100

    24

--- Scripts/script1.r
+++ Scripts/script1.r
@@ -1,5 +1,7 @@
 #zaladowanie bibliotek
 library(tm)
+library(hunspell)
+library(stringr)
 
 #zmiana katalogu roboczego
 workDir <- "D:\\KS\\TextMining"
@@ -14,10 +16,11 @@ dir.create(outputDir, showWarnings = FALSE)
 dir.create(workspacesDir, showWarnings = FALSE)
 
 #utworzenie korpusu dokumentow
-corpusDir <- paste(inputDir, 
-                   "Literatura - streszczenia - oryginaÅ‚",
-                   sep = "\\"
-                  )
+corpusDir <- paste(
+  inputDir, 
+  "Literatura - streszczenia - orygina³",
+  sep = "\\"
+)
 corpus <- VCorpus(
   DirSource(
     corpusDir,
@@ -40,6 +43,43 @@ stoplistFile <- paste(inputDir,
 stoplist <- readLines(stoplistFile, encoding = "UTF-8")
 corpus <- tm_map(corpus, removeWords, stoplist)
 corpus <- tm_map(corpus, stripWhitespace)
+
+#usuniecie em dash i 3/4
+removeChar <- content_transformer(function(x,pattern) gsub(pattern, "", x))
+corpus <- tm_map(corpus, removeChar, intToUtf8(8722))
+corpus <- tm_map(corpus, removeChar, intToUtf8(190))
+
+
+polish <- dictionary(lang="pl_PL")
+
+lemmatize <- function(text) {
+  simpleText <- str_trim(as.character(text))
+  parsedText <- strsplit(simpleText, split = " ")
+  newTextVec <- hunspell_stem(parsedText[[1]], dict = polish)
+  for (i in 1:length(newTextVec)) {
+    if (length(newTextVec[[i]]) == 0) newTextVec[i] <- parsedText[[1]][i]
+    if (length(newTextVec[[i]]) > 1) newTextVec[i] <- newTextVec[[i]][1]
+  }
+  newText <- paste(newTextVec, collapse = " ")
+  return(newText)
+}
+corpus <- tm_map(corpus, content_transformer(lemmatize))
+
+#usuniecie rozszerzen z nazw plikow
+cutExtensions <- function(document){
+  meta(document, "id") <- gsub(pattern = "\\.txt$", replacement = "", meta(document, "id"))
+  return(document)
+}
+corpus <- tm_map(corpus, cutExtensions)
+
+#eksport zawartosci korpusu do plikow tekstowych
+preprocessedDir <- paste(
+  outputDir, 
+  "Literatura - streszczenia - przetworzone",
+  sep = "\\"
+)
+dir.create(preprocessedDir)
+writeCorpus(corpus, path = preprocessedDir)
 
 #wyswietlenie zawartosci korpusu
 writeLines(as.character(corpus[[1]]))
